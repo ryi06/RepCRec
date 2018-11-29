62840903e05d63968bb080359806ec0a2004b4e9
Ren Yi <ryi06@hotmail.com>
2018-11-29 17:04:31 -0500

    Reorder files

---
+++ RepCRec_runner.py
@@ -0,0 +1,23 @@
+'''
+CSCI-GA 2434 Advanced Database Systems Final Project
+Replicated Concurrency Control and Recovery
+
+Authors:
+Ren Yi--ry708@nyu.edu
+Yanqiu Wu--yw1370@nyu.edu
+'''
+import argparse
+from RepCRec_local import Workflow
+
+
+parser = argparse.ArgumentParser('A distributed database with replicated concurrency control and recovery system')
+
+parser.add_argument('file_name', type=str, help="Path to sample test file")
+
+args = parser.parse_args()
+
+workflow = Workflow()
+
+workflow.file_name = args.file_name
+
+workflow.run()
