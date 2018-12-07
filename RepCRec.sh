ff30f84e72fd98e9a015243efb53b205734291b2
Ren Yi <ryi06@hotmail.com>
2018-12-06 20:00:45 -0500

    wrapper update

--- RepCRec.sh
+++ RepCRec.sh
@@ -9,9 +9,7 @@
 
 
 if [ "$1" = "" ]; then
-	echo "python RepCRec_runner.py test/test1.txt"
 	python RepCRec_runner.py test/test1.txt 
 else
-	echo "python RepCRec_runner.py ${1}"
 	python RepCRec_runner.py $1
 fi
